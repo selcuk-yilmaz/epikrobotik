a46d89c22c6e1eb0c8aa635e7d4c29a4e3e050a4
selcuklu <yilmax4@gmail.com>
2022-09-28 09:26:29 +0300

    ok

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Homes from "../pages/Homes";
 import Pages1 from "../pages/Pages1";
 import Pages2 from "../pages/Pages2";
 import Pages3 from "../pages/Pages3";
@@ -11,7 +10,7 @@ const AppRouter = () => {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Homes />} /> */}
+       
         <Route path="/" element={<Pages1 />} />
         <Route path="/pages2" element={<Pages2 />} />
         <Route path="/pages3" element={<Pages3 />} />
